50fa57983bb1bc1b3d45ae5c3f527efb19407328
Liskunov <d.liskunov@xlab-game.com>
2023-10-27 00:10:27 +0300

    Add scenes, scripts and .gitignore

---
+++ HearthRacing/Assets/Scripts/SimpleCarController.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SimpleCarController : MonoBehaviour
+{
+    public List<AxleInfo> axleInfos; // информация о каждой оси
+    public float maxMotorTorque; // максимальный крутящий момент
+    public float maxSteeringAngle; // максимальный угол поворота колес
+
+    public void ApplyLocalPositionToVisuals(WheelCollider collider)
+    {
+        if (collider.transform.childCount == 0)
+        {
+            return;
+        }
+
+        Transform visualWheel = collider.transform.GetChild(0);
+
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation;
+    }
+
+    public void FixedUpdate()
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+        }
+    }
+}
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor; // присоединено ли колесо к мотору?
+    public bool steering; // поворачивает ли это колесо?
+}

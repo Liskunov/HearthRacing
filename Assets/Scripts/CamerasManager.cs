e0f15cbe33622afdb9791295cabf0d3b8c7075d4
DaniilM353 <d.maksimov@xlab-game.com>
2023-12-11 21:36:47 +0300

    add camera manager

---
+++ Assets/Scripts/CamerasManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEditor.ShaderGraph.Drawing.Inspector.PropertyDrawers;
+
+public class CameraManager : MonoBehaviour
+{
+    public static class CameraNames
+    {
+        public const string Menu = "MenuCamera";
+        public const string Start = "StartCamera";
+        public const string Catalog = "CatalogCamera";
+        public const string Shop = "ShopCamera";
+        public const string Settings = "SettingsCamera";
+    }
+    
+    [SerializeField] private List<CinemachineVirtualCamera> m_cameras;
+    [SerializeField] private string m_startCamera;
+
+    private void Awake()
+    {
+        Activate(m_startCamera);
+    }
+
+    public void Activate(string cameraName)
+    {
+        foreach (var cam in m_cameras)
+        {
+            cam.enabled = cameraName == cam.name;
+        }
+    }
+}

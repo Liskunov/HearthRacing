21db5d39961eaa4e8770b109e61f55ecbd2853ac
DaniilM353 <d.maksimov@xlab-game.com>
2024-01-08 00:18:26 +0300

    not working ui

---
+++ Assets/Scripts/UI/UIItem.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIItem : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+   private RectTransform _rectTransform;
+   private Canvas _mainCanvas;
+   private CanvasGroup _canvasGroup;
+
+   private void start()
+   {
+      _rectTransform = GetComponent<RectTransform>();
+      _mainCanvas = GetComponentInParent<Canvas>();
+      _canvasGroup = GetComponent<CanvasGroup>();
+   }
+
+   public void OnDrag(PointerEventData eventData)
+   {
+      _rectTransform.anchoredPosition += eventData.delta / _mainCanvas.scaleFactor;
+   }
+
+   public void OnBeginDrag(PointerEventData eventData)
+   {
+      var slotTransform = _rectTransform.parent;
+      slotTransform.SetAsLastSibling();
+      _canvasGroup.blocksRaycasts = false;
+   }
+
+   public void OnEndDrag(PointerEventData eventData)
+   {
+      transform.localPosition = Vector3.zero;
+      _canvasGroup.blocksRaycasts = true;
+   }
+}

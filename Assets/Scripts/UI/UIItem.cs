f1a1f6928e9a71125d7a84aca4bac76e5e519526
DaniilM353 <d.maksimov@xlab-game.com>
2024-01-08 06:09:01 +0300

    71% drag and drop

--- Assets/Scripts/UI/UIItem.cs
+++ Assets/Scripts/UI/UIItem.cs
@@ -32,6 +32,6 @@ public class UIItem : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHa
    {
       transform.localPosition = Vector3.zero;
       _canvasGroup.blocksRaycasts = true;
-      
+
    }
 }
--- Assets/Scripts/UI/UISlot.cs
+++ Assets/Scripts/UI/UISlot.cs
@@ -8,12 +8,15 @@ public class UISlot : MonoBehaviour, IDropHandler
     public void OnDrop(PointerEventData eventData)
     {
         var otherItemTransform = eventData.pointerDrag.transform;
-        var parentT = otherItemTransform.parent;
+        if (transform.childCount != 0)
+        {
+            var parentT = otherItemTransform.parent;
         
-        var child = transform.GetChild(0);
-        child.transform.SetParent(parentT);
-        child.localPosition = Vector3.zero;
-
+            var child = transform.GetChild(0);
+            child.transform.SetParent(parentT);
+            child.localPosition = Vector3.zero;
+        }
+        
         otherItemTransform.SetParent(transform);
         otherItemTransform.localPosition = Vector3.zero;
     }

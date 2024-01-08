c78e6b3c1665d74edff84b5411846daa2708e00e
DaniilM353 <d.maksimov@xlab-game.com>
2024-01-08 05:16:52 +0300

    70% drag and drop

--- Assets/Scripts/UI/UISlot.cs
+++ Assets/Scripts/UI/UISlot.cs
@@ -8,6 +8,12 @@ public class UISlot : MonoBehaviour, IDropHandler
     public void OnDrop(PointerEventData eventData)
     {
         var otherItemTransform = eventData.pointerDrag.transform;
+        var parentT = otherItemTransform.parent;
+        
+        var child = transform.GetChild(0);
+        child.transform.SetParent(parentT);
+        child.localPosition = Vector3.zero;
+
         otherItemTransform.SetParent(transform);
         otherItemTransform.localPosition = Vector3.zero;
     }
